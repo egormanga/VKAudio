386dac1512e1b0c5d2b52cf5de872114f79576aa
egormanga <egorka000111000@gmail.com>
2019-02-28 20:48:24 +0300

    Fixed many things. Last commit before rewrite using Scurses.

--- VKAudio.py
+++ VKAudio.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # VK Audio Player
 
-import vlc, html, vaud, curses, locale, random
+import vlc, html, vaud, curses, struct
 from api import *
 from curses.textpad import Textbox
 from utils import *; logstart('VKAudio')
@@ -10,12 +10,37 @@ db.setfile(os.path.dirname(os.path.realpath(sys.argv[0]))+'/VKAudio.db')
 db.setbackup(False)
 tokens.require('access_token', 'messages,offline')
 
+class Mouse:
+	curses_map = {
+		1: curses.BUTTON1_PRESSED,
+		4: curses.BUTTON2_PRESSED,
+		2: curses.BUTTON3_PRESSED,
+	}
+
+	def __init__(self, h, w):
+		self.h, self.w = h, w
+		self.fd = open('/dev/input/mice', 'rb')
+		self.x = int()
+		self.y = int()
+	def getmouse(self):
+		b, dx, mdy = struct.unpack('Bbb', select.select((self.fd,), (), (), 0)[0][0].read(3))
+		self.x = Sint(self.x+dx).constrain(0, self.w-1)
+		self.y = Sint(self.y-mdy/2).constrain(0, self.h-1)
+		bstate = 0
+		for i in self.curses_map:
+			if (b & i): bstate |= self.curses_map[i]
+		return (0, round(self.x), round(self.y), 0, bstate)
+
 def main(stdscr, user_id):
-	curses.curs_set(False)
 	curses.use_default_colors()
 	stdscr.nodelay(True)
 	curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
 	curses.mouseinterval(0)
+
+	if (os.environ['TERM'] == 'linux'): mouse = None#Mouse(*stdscr.getmaxyx())
+	else: mouse = None
+	curses.curs_set(2*bool(mouse))
+	stdscr.leaveok(not mouse)
 
 	p = vlc.MediaPlayer()
 	p.get_instance().log_unset()
@@ -36,13 +61,12 @@ def main(stdscr, user_id):
 		ep.addstr(3, 2, 'Password:')
 		ep.refresh()
 		login, password = Textbox(stdscr.subpad(1, ew-13, ey+2, ex+12)), Textbox(stdscr.subpad(1, ew-13, ey+3, ex+12))
-		login.edit(); password.edit()
-		al_login(*map(str.strip, (login.gather(), password.gather())))
+		al_login(*map(str.strip, (login.edit(), password.edit())))
 	def loadDialogs():
-		nonlocal ll
+		nonlocal h, ll
 		if (ll <= 1): stdscr.addstr(0, 0, 'Loading'.center(stdscr.getmaxyx()[1]), curses.A_STANDOUT); stdscr.refresh()
 		if (len(l) < 2): l.insert(-1, {'name': '* My Audios', 'id': -1})
-		r = dialogs(offset=len(l)-1, extended=1)
+		r = dialogs(count=h-1, offset=len(l)-2, extended=1)
 		for i in r['items']:
 			if (i['conversation']['peer']['type'] == 'user'): u = S(r['profiles'])['id', i['conversation']['peer']['id']][0]; l.insert(-1, S(u)&{'name': ' '.join(S(u)@['first_name', 'last_name'])})
 			elif (i['conversation']['peer']['type'] == 'chat'): l.insert(-1, {'name': i['conversation']['chat_settings']['title'], 'id': i['conversation']['peer']['id']})
@@ -50,15 +74,15 @@ def main(stdscr, user_id):
 		l[-1] = bool(r['items'])
 		ll = len(l)-1
 	def loadAudios():
-		nonlocal ll
+		nonlocal h, ll
 		if (ll <= 1): stdscr.addstr(0, 0, 'Loading'.center(stdscr.getmaxyx()[1]), curses.A_STANDOUT); stdscr.refresh()
 		if (peer_id == -1): loadOwnAudios(); return
-		r = API.messages.getHistoryAttachments(peer_id=peer_id, media_type='audio', start_from=l[-1])
+		r = API.messages.getHistoryAttachments(peer_id=peer_id, media_type='audio', count=h, start_from=l[-1])
 		for i in S(r['items'])@['attachment']@['audio']:
 			if (len(l) < 2 or l[-2] != i): l.insert(-1, i)
 		l[-1] = r.get('next_from')
 		ll = len(l)-1
-	def loadOwnAudios(): # TODO: playlists, owners
+	def loadOwnAudios(): # TODO: playlists, owners, FIXME missing urls
 		nonlocal ll
 		if (not getvksid()): login()
 		r = API.audio.get(owner_id=user_id, offset=l[-1])
@@ -68,7 +92,7 @@ def main(stdscr, user_id):
 			'duration': i[5],
 			'is_hq': False, # TODO ???
 			'url': url_decoder.decode(i[2]),
-		})
+		})#; log(i[2]) # FIXME
 		l[-1] = str(r.get('nextOffset')) if (r['hasMore']) else None
 		ll = len(l)-1
 
@@ -105,15 +129,18 @@ def main(stdscr, user_id):
 		cs, cu = n, peer_id
 		p.stop()
 		stdscr.addstr(h, 1, 'Loading...')
-		try: p.set_mrl(l[n]['url']); p.play()
-		except: track = 'Error'
+		try: assert l[n]['url']; p.set_mrl(l[n]['url']); p.play()
+		except Exception: track = 'Error'
+		else: scrollToSelected()
 	def playNextTrack(force_next=False):
 		nonlocal l, n, cs, repeat, play_next
-		if (not repeat or force_next):
-			if (play_next): n = play_next.pop(0)
-			else: n = (cs+1) % len(l)
+		if (play_next): n = play_next.pop(0)
+		elif (not repeat or force_next): n = (cs+1) % len(l)
 		playTrack()
-		scrollToSelected()
+	def playPrevTrack():
+		nonlocal l, n, cs, repeat, play_next
+		n = max(n-1, 0)
+		playTrack()
 
 	def strfTime(t): return time.strftime('%H:%M:%S', time.gmtime(t)).lstrip('0').lstrip(':')
 	def debugOut(*s, sep=' '): s = sep.join(map(str, s)); stdscr.addstr(0, (stdscr.getmaxyx()[1]-len(s))//2-1, s, curses.A_STANDOUT)
@@ -151,8 +178,9 @@ def main(stdscr, user_id):
 					if (type(l[i]) == str): stdscr.addstr(i-t, 0, 'Load more...', curses.A_STANDOUT*(i==n)); continue
 					elif (not l[i]): stdscr.addstr(i-t, 0, 'End.', curses.A_STANDOUT*(i==n)); ll = len(l)-2; continue
 					title = '%s — %s' % (l[i]['artist'], l[i]['title'])
-					title_attrs = ' '.join((str(play_next.index(i)+1) if (i in play_next) else '', 'HQ'*l[i]['is_hq'], strfTime(l[i]['duration'])))
-					stdscr.addstr(i-t, 0, S(title).fit(w-len(title_attrs)-2)+' '+title_attrs.rjust(w-len(title)-1), curses.A_STANDOUT*(i==n) | curses.A_BOLD*(i==cs and peer_id==cu))
+					title_attrs = (str(play_next.index(i)+1)+' ' if (i in play_next) else '')+'HQ '*l[i]['is_hq']+strfTime(l[i]['duration'])
+					title = S(title).fit(w-len(title_attrs)-2)
+					stdscr.addstr(i-t, 0, title+' '+title_attrs.rjust(w-len(title)-1), curses.A_STANDOUT*(i==n) | curses.A_BOLD*(i==cs and peer_id==cu))
 			pl = max(0, p.get_length())
 			pp = min(1, p.get_position())
 			pgrstr = f"{strfTime(pl*pp/1000)}/{strfTime(pl/1000)} %s {time.strftime('%X')}"
@@ -196,16 +224,23 @@ def main(stdscr, user_id):
 			elif (c == ord('q') or c == ord('\033') or c == curses.KEY_BACKSPACE or c == curses.KEY_EXIT): goBack()
 			elif (c == ord('a')):
 				playNextTrack(force_next=True)
+			elif (c == ord('z')):
+				playPrevTrack()
 			elif (c == ord('s')):
 				p.stop()
 				cs = -1
 			elif (c == ord('r')):
 				toggleRepeat()
 			elif (c == ord('n')):
-				play_next.append(n)
-			elif (c == curses.KEY_MOUSE):
-				try: id, x, y, z, bstate = curses.getmouse()
-				except curses.error: id = x = y = z = bstate = 0
+				if (n in play_next): play_next.remove(n)
+				else: play_next.append(n)
+			elif (c == ord('b')):
+				if (cs != -1): n = cs
+				scrollToSelected()
+			elif (c == curses.KEY_MOUSE or mouse):
+				try: id, x, y, z, bstate = (mouse or curses).getmouse()
+				except (curses.error, IndexError): id = x = y = z = bstate = 0
+				if (mouse): stdscr.move(y, x)
 				if (bstate == curses.BUTTON4_PRESSED): t = max(t-3, 0)
 				elif (bstate == curses.REPORT_MOUSE_POSITION and ll > h): t = min(t+3, ll-h+1)
 				else:
@@ -225,14 +260,15 @@ def main(stdscr, user_id):
 							toggleRepeat()
 
 			stdscr.refresh()
+			curses.napms(5) # TODO FIXME
 	except KeyboardInterrupt as ex: return
 	finally: p.stop()
+
+# TODO: fix enormous CPU usage!
 
 if (__name__ == '__main__'):
 	logstarted()
 	db.load()
-	user()
-	#if ('xterm' in os.environ['TERM']): os.environ['TERM'] = 'xterm-1002' # mouse movement
 	exit(curses.wrapper(main, user()[0]['id']))
 else: logimported()
 
